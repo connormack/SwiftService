6af01c7efec536f38bd9b99ef6bd006b470f74e4
Connor Mackintosh <connor_mackintosh@hotmail.co.uk>
2023-02-21 14:11:09 +0000

    2.3 commit

--- admin/manager-admin.php
+++ admin/manager-admin.php
@@ -3,6 +3,25 @@
     <div class="main-content">
     <div class ="wrapper">
             <h1>Manage admin</h1>
+
+            <table class ="tbl-full">
+                <tr>
+                <th>S.N</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>CM</td>
+                    <td>CM1</td>
+                    <td>
+                        Update Admin
+                        Ddelete Admin
+                    </td>
+                </tr>
+            </table>
+
         </div>
 </div>
 
