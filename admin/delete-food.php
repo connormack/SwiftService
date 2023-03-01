bbfc3f0951e77db44b0af01b5ded59df33586de9
Connor Mackintosh <connor_mackintosh@hotmail.co.uk>
2023-03-01 14:39:08 +0000

    update food added

---
+++ admin/delete-food.php
@@ -0,0 +1,62 @@
+<?php
+include('../config/constants.php');
+
+// echo "delete";
+
+if(isset($_GET['id']) && isset($_GET['image_name']))
+{
+    // get the value and delete
+    // echo "delete2";
+
+    // get id and image name
+    $id = $_GET['id'];
+    $image_name = $_GET['image_name'];
+
+    // remove image is there is one 
+    // check image and delete if available
+    if($image_name != "")
+    {
+        //image found so remove it
+        $path = "../images/food/".$image_name;
+        // remove image
+        $remove = unlink($path);
+
+        // check if image is removed
+        if($remove==false)
+        {
+            // failed to remove image
+            $_SESSION['remove'] = "<div class='fail'>Failed to remove image.</div>";
+            header('location:'.SITEURL.'admin/manage-food.php');
+            die();
+        }
+    }
+    // delete food from DB
+    $sql = "DELETE FROM tbl_food WHERE id=$id";
+
+    // run the query
+    $res = mysqli_query($conn, $sql);
+
+    // check if query has run and set message 
+    if($res==true)
+    {
+        // query run and food deleted
+        $_SESSION['delete'] = "<div class='success'>Food item deleted successfully.</div>";
+        header('location:'.SITEURL.'admin/manage-food.php');
+    }
+    else{
+        // failed to delete food
+        $_SESSION['delete'] = "<div class='fail'>Failed to delete food item.</div>";
+        header('location:'.SITEURL.'admin/manage-food.php');
+    }
+
+
+    // redirect to manage food with session message
+}
+else{
+    // redirect to manage food
+    $_SESSION['unauthorised'] = "<div class='fail'>Unauthorized access.</div>";
+    header('location:'.SITEURL.'admin/manage-food.php');
+
+}
+
+?>
--- admin/manage-food.php
+++ admin/manage-food.php
@@ -11,6 +11,22 @@
                 echo $_SESSION['add'];
                 unset($_SESSION['add']);
             }
+            if(isset($_SESSION['delete']))
+            {
+                echo $_SESSION['delete'];
+                unset($_SESSION['delete']);
+            }
+            if(isset($_SESSION['upload']))
+            {
+                echo $_SESSION['upload'];
+                unset($_SESSION['upload']);
+            }
+            if(isset($_SESSION['unauthorised']))
+            {
+                echo $_SESSION['unauthorised'];
+                unset($_SESSION['unauthorised']);
+            }
+
 
             ?>
 
@@ -78,7 +94,7 @@
                         <td><?php echo $active;?></td>
                         <td>
                         <a href="#" class="btn-secondary">Update Food</a>
-                        <a href="#" class="btn-danger">Delete Food</a>
+                        <a href="<?php echo SITEURL; ?>admin/delete-food.php?id=<?php echo $id;?>&image_name=<?php echo $image_name;?>"class="btn-danger">Delete Food</a>
                         </td>
                     </tr>
                         <?php
