89aab3fba9e85e4b2690cb7f35753a64a7e3d7a2
Connor Mackintosh <connor_mackintosh@hotmail.co.uk>
2023-03-05 19:13:57 +0000

    Admin dashboard completed

--- admin/index.php
+++ admin/index.php
@@ -17,27 +17,83 @@
             <br>
 
             <div class = "col-4 text-center" >
-                <h1>5</h1>
+
+            <?php 
+                    // create sql query to get all the categories
+                    $sql2 = "SELECT * FROM tbl_category";
+
+                    // run query
+                    $res2 = mysqli_query($conn, $sql2);
+
+                    // count the rows
+                    $count2 = mysqli_num_rows($res2);
+
+            ?>
+
+                <h1><?php echo $count2 ?></h1>
+                <br>
+                Menu Items
+            </div>
+
+            <div class = "col-4 text-center" >
+
+                <?php 
+                    // create sql query to get all the food
+                    $sql = "SELECT * FROM tbl_food";
+
+                    // run query
+                    $res = mysqli_query($conn, $sql);
+
+                    // count the rows
+                    $count = mysqli_num_rows($res);
+
+                    ?>
+
+                <h1><?php echo $count; ?></h1>
                 <br>
                 Categories
             </div>
 
             <div class = "col-4 text-center" >
-                <h1>5</h1>
+
+            
+            <?php 
+                    // create sql query to get all the total orders
+                    $sql3 = "SELECT * FROM tbl_order";
+
+                    // run query
+                    $res3 = mysqli_query($conn, $sql3);
+
+                    // count the rows
+                    $count3 = mysqli_num_rows($res3);
+
+            ?>
+                <h1><?php echo $count3 ?></h1>
                 <br>
-                Categories
+                Total orders
             </div>
 
             <div class = "col-4 text-center" >
-                <h1>5</h1>
-                <br>
-                Categories
-            </div>
+                
+            <?php 
+                    // create sql query to get all the total revenue
+                    // aggregate sql sum function 
+                    $sql4 = "SELECT SUM(total) AS Total FROM tbl_order WHERE status='Completed'";
 
-            <div class = "col-4 text-center" >
-                <h1>5</h1>
+                    // run query
+                    $res4 = mysqli_query($conn, $sql4);
+
+                    // get the value
+                    $row4 = mysqli_fetch_assoc($res4);
+
+                    // get total revenue
+                    $total_revenue = $row4['Total'];
+
+
+                    ?>
+                <h1><?php echo "£" . $total_revenue ?></h1>
                 <br>
-                Categories
+                Total Revenue
             </div>
 
             <div class="clearfix"></div>
--- admin/manage-order.php
+++ admin/manage-order.php
@@ -15,21 +15,21 @@
             ?>
             <br>
             <br>
-
+            <!-- &nbsp; white space tag  -->
             <table class="tbl-full">
                 <tr>
-                <th>S.N</th>
-                <th>Food</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Total</th>
-                <th>Order Date</th>
-                <th>Status</th>
-                <th>Customer Name</th>
-                <th>Customer Contact</th>
-                <th>Customer Email</th>
-                <th>Table Number</th>
-                <th>Actions</th>
+                <th>S.N &nbsp; &nbsp; </th>
+                <th>Food  &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;   </th>
+                <th>Price &nbsp; &nbsp; </th>
+                <th>Quantity &nbsp; </th>
+                <th>Total&nbsp; &nbsp; &nbsp; </th>
+                <th>Order Date &nbsp; &nbsp;  </th>
+                <th>Status &nbsp; </th>
+                <th>Customer Name &nbsp; &nbsp; </th>
+                <th>Customer Contact &nbsp; </th>
+                <th>Customer Email &nbsp; </th>
+                <th>Table Number &nbsp; </th>
+                <th>Actions &nbsp;  </th>
                 </tr>
 
                 <?php
