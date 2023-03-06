8a46df8c56a45225e5b28ad4e0ed71fd51c3c869
Connor Mackintosh <connor_mackintosh@hotmail.co.uk>
2023-03-06 00:58:43 +0000

    Establishment code section added

---
+++ landing.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Website</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Irish Grover' rel='stylesheet'>
+</head>
+
+<body class= "landing-background">
+    <div class ="main content">
+        <div class="wrapper">
+            <div class = "container2">
+            <img src="http://localhost/SDP/images/landing/Pasta.jpg" alt="Pasta"  class="img1"/>
+            <img src="http://localhost/SDP/images/landing/Taco.jpg" alt="Taco"  class="img1"/>
+            <img src="http://localhost/SDP/images/landing/Ramen.jpg" alt="Ramen"  class="img1"/>
+            <img src="http://localhost/SDP/images/landing/Burger.jpg" alt="Burger"  class="img1"/>
+            <img src="http://localhost/SDP/images/landing/Curry.jpg" alt="Curry"  class="img1"/>
+            <img src="http://localhost/SDP/images/landing/Pizza.jpg" alt="Pizza"  class="img1"/>
+
+            </div>
+            <div>
+            <h1 class = "swiftService-text">SwiftService</h1>
+            <h2 class="landing-text">Enter Establishment Code:</h2>
+            <input type="number" class="landing-input"> 
+            <div style="text-align: center;">
+            <br>
+            <button class="landing-button">Submit</button>
+            </div>
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+</body>
+</html>
