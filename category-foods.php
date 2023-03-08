679552aa94e1bccfe21c1856d6a2cb181da8371e
Connor Mackintosh <connor_mackintosh@hotmail.co.uk>
2023-03-08 00:27:08 +0000

    update images v2.0

--- category-foods.php
+++ category-foods.php
@@ -81,7 +81,7 @@
                             {
                                 // image available
                                 ?>
-                                <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
+                                <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" height="135" width="125" class="img-responsive img-curve">
                                 <?php
                             }
 
